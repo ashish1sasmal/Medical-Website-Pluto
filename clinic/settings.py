aba88447362754ffe7234fcc6825ed0e9f5819f4
ashish1sasmal <ashishsasmal1@gmail.com>
2019-08-31 22:18:57 +0530

    navbar config

--- clinic/settings.py
+++ clinic/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'fe5586c4827afda961a31c2965c0bfd4d9755fd536832030'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
